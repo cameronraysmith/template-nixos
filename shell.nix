9ab65c4981f0cf95bdc6e101808e53d306c6166b
Cameron Smith <camrn86@gmail.com>
2022-02-08 04:10:28 +0000

    update shell.nix

--- shell.nix
+++ shell.nix
@@ -1,14 +1,22 @@
 let 
   pkgs = import <nixpkgs> {};
   nixos-generators = import (builtins.fetchTarball https://github.com/nix-community/nixos-generators/archive/master.tar.gz);
-
+  testScript = pkgs.writeScriptBin "testscript" ''
+    echo "Test Script";
+  '';
 in with pkgs;
 mkShell {
+  name = "base-nix-shell";
 
   nativeBuildInputs = [
     direnv
     niv
     nixos-generators
+  ];
+
+  buildInputs = [
+    figlet
+    testScript
   ];
 
   NIX_ENFORCE_PURITY = true;
