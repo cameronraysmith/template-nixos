2c4340913b770621aeccb3bdb42e49125201ec99
Cameron Smith <camrn86@gmail.com>
2022-02-08 04:10:58 +0000

    add nix flakes to configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -6,6 +6,13 @@
   services.openssh.listenAddresses = [{ addr = "0.0.0.0"; port = 22; }];
   services.openssh.passwordAuthentication = true;
 
+  nix = {
+    package = pkgs.nixUnstable; # or versioned attributes like nix_2_4
+    extraOptions = ''
+      experimental-features = nix-command flakes
+    '';
+  };
+
   environment.systemPackages = with pkgs; [
     curl
     gitAndTools.gitFull
